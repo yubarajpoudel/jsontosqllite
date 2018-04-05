7b4bb90669aab37344e7470c09f7a12199a80a36
yubaraj poudel <yubarajpoudel708@gmail.com>
2018-04-05 21:39:11 +0545

    organized in functions

--- jsontosqlite.py
+++ jsontosqlite.py
@@ -11,46 +11,62 @@ import sqlite3
 
 def __convert(*args):
 	try:
-		conn = sqlite3.connect(args[2])
-		c = conn.cursor()
-		print(sqlite3.version)	
-		print("....................................\n")
-		print("Choose the option\n")
-		print("1. Create new table\n")
-		print("2. Insert data into table\n")
-		print("3. delete table\n")
-
-		print(".............................")
-
-		choice = int(input("Enter your choice"))
+		__showOptions()
+		choice = int(input("Enter your choice\t"))
 		if choice == 1:
-			tableName = input("Enter the table name :\t")
-			schemas= input("Enter the schema separated by space ( for eg name,phone):\t")
-			print("tableName = {}, schemas = {}".format(tableName, schemas))
-			# Create table
-			sql = "CREATE TABLE {}(".format(tableName)
-			first = True
-			for schema in schemas.split(","):
-				if not first:
-					sql = sql + ","
-
-				sql = sql + "{} text".format(schema)
-				first = False
-
-			sql = sql+")"
-			print(sql)
-			c.execute(sql)
-			conn.commit()
-			conn.close()
-			print("{} created successfully ".format(tableName))			
+			__createTable(args[2])
 			pass
 		elif choice == 2:
 			pass
 		elif choice == 3:
 			pass
+		elif choice == 4:
+			pass
+		elif choice == 5:
+			pass
+		elif choice == 9:
+			exit()
 	except:
 	 	print("File exception")
 
+def __showOptions():
+	print("....................................\n")
+	print("Choose the option\n")
+	print("1. Create new table\n")
+	print("2. Insert data into table\n")
+	print("3. Delete table\n")
+	print("4. Alter table\n")
+	print("5. Create Tables from jsonfile \n")
+	print("9. Press 9 for exit\n")
+	print(".............................")
+
+def __createTable(dbPath):
+	conn = sqlite3.connect(dbPath)
+	c = conn.cursor()
+
+	nextTable = 'y'
+	while nextTable.lower() == 'y':
+		tableName = input("Enter the table name :\t")
+		schemas= input("Enter the schema separated by space (for eg name,phone):")
+		print("tableName = {}, schemas = {}".format(tableName, schemas))
+		# Create table
+		sql = "CREATE TABLE {}(".format(tableName)
+		first = True
+		for schema in schemas.split(","):
+			if not first:
+				sql = sql + ","
+				
+			sql = sql + "{} text".format(schema)
+			first = False
+		sql = sql+")"
+		print(sql)
+		c.execute(sql)
+		print("{} created successfully \n".format(tableName))
+		nextTable = input("Do you want to add more tables ? (Y | N):\t")
+
+	conn.commit()
+	conn.close()
+	
 def main():
 	print("arguments lenght = %s"%(str(len(sys.argv))))
 	try:
