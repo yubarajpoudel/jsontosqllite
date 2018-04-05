829936f5046b37399cf5cbe0a7dc79bacb906505
yubaraj poudel <yubarajpoudel708@gmail.com>
2018-04-06 00:06:01 +0545

    table made create from json

--- jsontosqlite.py
+++ jsontosqlite.py
@@ -8,6 +8,8 @@ prvide the name of the output file
 """
 import sys
 import sqlite3
+import json
+import os
 
 def __convert(*args):
 	try:
@@ -16,6 +18,7 @@ def __convert(*args):
 			__showOptions()
 			choice = int(input("Enter your choice "))
 			if choice == 1:
+				# print(args[2])
 				__createTable(args[2])
 				pass
 			elif choice == 2:
@@ -25,16 +28,24 @@ def __convert(*args):
 			elif choice == 4:
 				pass
 			elif choice == 5:
+				tableJSON = input("Enter the file name: ")
+				if tableJSON.lower().endswith(".json"):
+					with open(tableJSON, "r") as inputTableJSON:
+						for table in json.load(inputTableJSON):
+							__createTableWith(table['name'], table['schemas'], args[2])
+				else:
+					print("Invalid json file. The file name must endswith .json format")
 				pass
 			elif choice == 9:
 				sys.exit(0)
 				pass
 
 			option = input("Enter m to continue: ")
-	except:
-	 	print("File exception")
+
+	except Exception as e: print(e)
 
 def __showOptions():
+	os.system("clear")
 	print("....................................\n")
 	print("Choose the option\n")
 	print("1. Create new table\n")
@@ -46,44 +57,47 @@ def __showOptions():
 	print(".............................")
 
 def __createTable(dbPath):
-	conn = sqlite3.connect(dbPath)
-	c = conn.cursor()
-
 	nextTable = 'y'
 	while nextTable.lower() == 'y':
 		tableName = input("Enter the table name : ")
 		schemas= input("Enter the schema separated by space (for eg name,phone):")
-		print("tableName = {}, schemas = {}".format(tableName, schemas))
-		# Create table
-		sql = "CREATE TABLE {}(".format(tableName)
-		first = True
-		for schema in schemas.split(","):
-			if not first:
-				sql = sql + ","
-				
-			sql = sql + "{} text".format(schema)
-			first = False
-		sql = sql+")"
-		print(sql)
-		c.execute(sql)
-		print("{} created successfully \n".format(tableName))
+		# print("tableName = {}, schemas = {}".format(tableName, schemas))
+		__createTableWith(tableName, schemas, dbPath)
 		nextTable = input("Do you want to add more tables ? (Y | N): ")
 
+
+def __createTableWith(tableName, schemas, dbPath):
+	conn = sqlite3.connect(dbPath)
+	c = conn.cursor()
+
+	# Create table
+	sql = "CREATE TABLE {}(".format(tableName)
+	first = True
+	for schema in schemas.split(","):
+		if not first:
+			sql = sql + ","
+				
+		sql = sql + "{} text".format(schema)
+		first = False
+	sql = sql+")"
+	print(sql)
+	c.execute(sql)
+	print("{} created successfully \n".format(tableName))
 	conn.commit()
 	conn.close()
 
 def main():
-	print("arguments lenght = %s"%(str(len(sys.argv))))
+	# print("arguments lenght = %s"%(str(len(sys.argv))))
 	try:
 		if len(sys.argv) == 3:
-			print("valid")
-			print(sys.argv[1])
+			# print("valid")
+			# print(sys.argv[1])
 			if(sys.argv[1].lower().endswith('.json')):
 				with open(sys.argv[1], "r") as inputJSON:
 					data = inputJSON.read()
 					
 				dbName = input("Enter Preferred DatabaseName ")
-				print("Database Name = {}".format(dbName))
+				# print("Database Name = {}".format(dbName))
 				destination = sys.argv[2]
 				# with open(destination, "w") as outputFile:
 				__convert(data, dbName, destination)
